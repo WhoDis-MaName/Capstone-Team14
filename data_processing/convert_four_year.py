d17909a1f041a9e551e77486fc6f127457c16a0d
joshgryzen <58818027+joshgryzen@users.noreply.github.com>
2025-04-08 19:55:55 -0500

    progress on optimizer with groups and weights

--- data_processing/convert_four_year.py
+++ data_processing/convert_four_year.py
@@ -16,6 +16,8 @@ def json_to_clingo(json_file, output_file):
 
     clingo_facts = []
 
+    planned_classes = []
+
     year_id = 1
     module_id = 0
     for year, semesters in data.items():
@@ -30,17 +32,18 @@ def json_to_clingo(json_file, output_file):
                 clingo_facts.append(module_fact)
 
                 for course in course_codes:
-                    course_fact = f"course({course}, m{module_id}, {credits})."
+                    course_fact = f"planned_course({course}, m{module_id}, {credits})."
                     clingo_facts.append(course_fact)
+                    planned_classes.append(course)
 
                 module_id += 1
         year_id += 1
-
+    clingo_facts.append(f"planned_classes({'; '.join(planned_classes)}).")
     with open(output_file, "w") as file:
         file.write("\n".join(clingo_facts))
 
 
 # Example usage
-json_file = r"C:\Users\cjgry\Documents\Capstone\Capstone-Team14\data_files\four_year_plan\fourYearPlan.json"  # Change this to your actual file path
+json_file = r"C:\Users\Josh\Documents\GitHub\Capstone-Team14\data_files\four_year_plan\fourYearPlan.json"  # Change this to your actual file path
 output_file = json_file.replace(".json", ".lp")  # Output file
 json_to_clingo(json_file, output_file)
